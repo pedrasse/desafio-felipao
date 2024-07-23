55fdad2fb16c9fa12cea953eceb8aee9c5c4e797
Pedrasse <gabrielpedrasse07@gmail.com>
2024-07-22 21:11:25 -0300

    DESAFIO XP

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
-let nome = "";
-console.log("oi");
+let nome = "Gabriel";
+let xp = 99999;
+
+if (xp < 1000) {
+    xp = "FERRO"
+    } else if (xp < 2000) {
+        xp = "BRONZE"
+        } 
+        else if (xp < 5000){
+            xp = "PRATA"
+        }
+        else if (xp < 7000){
+            xp = "OURO"
+        }
+        else if (xp < 8000){
+           xp = "PLATINA"
+        }
+        else if (xp < 9000){
+            xp = "ASCENDENTE"
+        }
+        else if (xp < 10000){
+            xp = "IMORTAL"
+        }
+        else if (xp > 10001){
+            xp = "RADIANTE"
+        } else {
+            xp = null
+            console.log("ERRO, VOCÊ NAO TEM XP")
+        }
+
+
+
+
+console.log("O Herói de nome " + nome + " está no nível de " + xp )
